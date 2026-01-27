d591b367a08fae68ebacb094b9e0abc43099e62c
doppel-prof <matthias.urset@skf.com>
2026-01-27 14:43:30 +0100

    save_mult_csv

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ Path=os.getcwd()
 Repo="test"
 File_name="test.csv"
 Lx=[0,1,2,3,4]
-Ly=[10,11,12,13,14]
+My=[[10,11,12,13,14],[20,21,22,13,24]]
 
-fu.save_tuple_csv(Lx,Ly,Path,Repo,File_name)
+fu.save_mult_csv(Lx,My,Path,Repo,File_name)
