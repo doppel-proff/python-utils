ba356ce0741e3c7cbc850c67834de2c87c68da31
doppel-prof <matthias.urset@skf.com>
2026-01-27 17:21:09 +0100

    added save parquet

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import os
 
 Path=os.getcwd()
 Repo="test"
-File_name="test.csv"
+File_name="test.parquet"
 Lx=[0,1,2,3,4]
 My=[[10,11,12,13,14],[20,21,22,13,24]]
 
-fu.save_mult_csv(Lx,My,Path,Repo,File_name)
+fu.save_mult_pqt(Lx,My,Path,Repo,File_name)
+fu.pqt_to_csv(Path, Repo, File_name)
